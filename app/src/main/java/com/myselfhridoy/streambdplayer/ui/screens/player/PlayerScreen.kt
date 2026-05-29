bb560038b6db058ac818a854c770ff7b4d220283
Hridoy Matber <hridoy200065@diit.edu.bd>
2026-05-29 22:33:13 +0600

    Fix unresolved reference RoundedCornerShape in PlayerScreen

--- app/src/main/java/com/myselfhridoy/streambdplayer/ui/screens/player/PlayerScreen.kt
+++ app/src/main/java/com/myselfhridoy/streambdplayer/ui/screens/player/PlayerScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
